363623c4ca61c012967dc8e58222dd101c855be2
Atharva pawar <atharvapawar2018@gmail.com>
2024-02-08 12:44:16 +0530

    Integrate API into Movielist component

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,4 +1,31 @@
-function MovieList(){
-    return<div>MovieList</div>
+import './movielist.css';
+import { useState,useEffect, useCallback } from 'react';
+import { getData } from '../api';
+
+function MoviesList(){
+        
+        const [data, setData] = useState([]);
+
+        useEffect(() => {
+            getData().then( res => setData(res.data.results))
+        },[]);
+    
+        console.log(data);
+    return(
+    <div className="container">
+         <div className="episodes">
+            {
+            data.map((item) =>
+            <div>
+             <div>Episode {item.episode_id}      {item.title}          {item.release_date} </div> 
+             <hr className="line" />   
+            </div>     
+            )
+            }
+         </div>
+         <div className="partition"></div>
+    </div>    
+    );
 }
-export default MovieList;
+
+export default MoviesList;
